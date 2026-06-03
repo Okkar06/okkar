e62f59f2c8e2c4e0aeb92c0441e3d4046a60586a
Okkar06 <oakkarhein.eduvalor.2023c@gmail.com>
2026-06-03 16:09:10 +0800

    feat: configure render settings and enhance server static file serving

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const env = require('./config/env')
@@ -18,5 +19,9 @@ app.use((error, _req, res, _next) => {
   console.error(error?.message || 'internal server error')
   res.status(500).json({ error: 'internal server error' })
 })
+
+const clientDist = path.join(__dirname, '..', '..', 'client', 'dist')
+app.use(express.static(clientDist))
+app.get('*', (_req, res) => res.sendFile(path.join(clientDist, 'index.html')))
 
 module.exports = app
